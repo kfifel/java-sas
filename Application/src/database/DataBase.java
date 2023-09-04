8719c4cb6537104ed18c221c70ea718a9442321a
khalid fifel <fifle.khalid99@gmail.com>
2023-09-04 10:51:00 +0100

    #SAS-1 add database class

---
+++ Application/src/database/DataBase.java
@@ -0,0 +1,42 @@
+package database;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class DataBase {
+    private static final String DB_NAME = "library-management";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+    private static final String SERVER_NAME = "localhost";
+    private static Connection conn = null;
+
+    private DataBase() {
+    }
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                conn = DriverManager.getConnection("jdbc:mysql://localhost/library-management", "root", "");
+            } catch (SQLException | ClassNotFoundException var1) {
+                System.out.println("Error in connection to the database: " + var1);
+            }
+        }
+
+        return conn;
+    }
+
+    public static void disconnect() {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+            }
+        } catch (SQLException var1) {
+            System.out.println("Disconnection error: " + var1.getMessage());
+        }
+
+        conn = null;
+    }
+}
