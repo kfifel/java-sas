a6f8337c0652d65e7cfe85eee805f71a90885549
khalid fifel <fifle.khalid99@gmail.com>
2023-09-08 15:41:11 +0100

    add Authentication service and implement login

---
+++ Application/src/security/Authentication.java
@@ -0,0 +1,50 @@
+package security;
+
+import database.DataBase;
+import model.Librarian;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Authentication {
+    public static Librarian librarian;
+
+    public static boolean login(String email, String password) throws SQLException {
+        Connection connection = DataBase.getConnection();
+        String query = "SELECT id, firstname, lastname ,email FROM librarian WHERE email = ? AND password = ? ";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+        preparedStatement.setString(1, email);
+        preparedStatement.setString(2, password);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        if (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String firstname = resultSet.getString("firstname");
+            String lastname = resultSet.getString("lastname");
+            String userEmail = resultSet.getString("email");
+
+            Librarian librarian1 = new Librarian();
+            librarian1.setEmail(userEmail);
+            librarian1.setFirstname(firstname);
+            librarian1.setLastname(lastname);
+            librarian1.setId(id);
+
+            Authentication.librarian = librarian1;
+
+            resultSet.close();
+            preparedStatement.close();
+
+            return true;
+        } else {
+            resultSet.close();
+            preparedStatement.close();
+
+            return false;
+        }
+
+    }
+}
